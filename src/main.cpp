fb82a07d603185126219abd7a554aae1cf6f9f43
grubbauer <raphael.grubbauer@hotmail.com>
2024-07-28 20:21:12 +0200

    Expanded code

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,43 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL2/SDL.h>
 
 const int SCR_WIDTH = 640;
 const int SCR_HEIGHT = 480;
 
-SDL_Window* gWindow;
-SDL_Renderer* gRenderer;
+SDL_Window *gWindow;
+SDL_Renderer *gRenderer;
 
 void initialise();
 void quit();
 
+int main(int argc, char *argv[]) {
+    initialise();
+
+    bool stop = false;
+    SDL_Event e;
+
+    while (!stop) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                stop = true;
+            }
+        }
+    }
+
+    quit();
+    return 0;
+}
+
 void initialise() {
     SDL_Init(SDL_INIT_VIDEO);
-    gWindow = SDL_CreateWindow("MathOrDeath v.0.0.1-2-nf",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,SCR_WIDTH,SCR_HEIGHT,SDL_WINDOW_SHOWN);
+    gWindow = SDL_CreateWindow("MathOrDeath v.0.0.1-3", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCR_WIDTH, SCR_HEIGHT, SDL_WINDOW_SHOWN);
     gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 }
 
-int main(int argc, char* argv[]) {
-    return 0;
+void quit() {
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+
+    SDL_Quit();
 }
