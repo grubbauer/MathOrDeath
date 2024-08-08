db1c81ba0e880b14dc054df9948d9f43774aa9b6
grubbauer <raphael.grubbauer@hotmail.com>
2024-08-08 11:11:18 +0200

    Added ogg file playability

--- src/main.cpp
+++ src/main.cpp
@@ -125,7 +125,7 @@ void initialise() {
 
 void loadAssets() {
     gTestBackground.loadFromFile("res/img/misc/background-0001.png");   
-    sMusic = Mix_LoadMUS("res/sfx/music/test.wav");
+    sMusic = Mix_LoadMUS("res/sfx/music/test.ogg");
 }
     	
 void quit() {
