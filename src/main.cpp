0e379e917b55766966b0ca40b32fb8ffea0617f1
grubbauer <raphael.grubbauer@hotmail.com>
2024-08-18 18:13:19 +0200

    Updated format

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
-#include "random.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
+
 #include <iostream>
+#include <string>
+
+#include "random.h"
 
 // Window variables
 int SCR_WIDTH = 0;
@@ -14,10 +16,10 @@ int SCR_HEIGHT = 0;
 // General global variables
 const std::string VERSION = "v.0.0.1-16";
 
-SDL_Window* gWindow = NULL;
-SDL_Renderer* gRenderer = NULL;
+SDL_Window *gWindow = NULL;
+SDL_Renderer *gRenderer = NULL;
 
-Mix_Music* sMusic = NULL;
+Mix_Music *sMusic = NULL;
 
 int Operator;
 
@@ -28,165 +30,169 @@ void quit();
 
 // Texture class
 class cTexture {
-public:
-    cTexture(); // Constructor
-    ~cTexture(); // Destructor
-    
-    void loadFromFile(std::string path);
-    void free();
-    void render(int x, int y, int w, int h);
+ public:
+  cTexture();   // Constructor
+  ~cTexture();  // Destructor
 
-    int getWidth();
-    int getHeight();
-private:
-    SDL_Texture* mTexture;
-    int mHeight;
-    int mWidth;
+  void loadFromFile(std::string path);
+  void free();
+  void render(int x, int y, int w, int h);
+
+  int getWidth();
+  int getHeight();
+
+ private:
+  SDL_Texture *mTexture;
+  int mHeight;
+  int mWidth;
 };
 
 // Class objects
 cTexture gTestBackground;
 
 cTexture::cTexture() {
-    mTexture = NULL;
-    mHeight = 0;
-    mWidth = 0;
+  mTexture = NULL;
+  mHeight = 0;
+  mWidth = 0;
 }
 
-cTexture::~cTexture() {
-    free();
-}
+cTexture::~cTexture() { free(); }
 
 void cTexture::loadFromFile(std::string path) {
-    free();
-    SDL_Surface* oldSurface = IMG_Load(path.c_str());
-    if (oldSurface != NULL) {
-        mTexture = SDL_CreateTextureFromSurface(gRenderer, oldSurface);
-        mWidth = oldSurface->w;
-        mHeight = oldSurface->h;
-        SDL_FreeSurface(oldSurface);
-    }
+  free();
+  SDL_Surface *oldSurface = IMG_Load(path.c_str());
+  if (oldSurface != NULL) {
+    mTexture = SDL_CreateTextureFromSurface(gRenderer, oldSurface);
+    mWidth = oldSurface->w;
+    mHeight = oldSurface->h;
+    SDL_FreeSurface(oldSurface);
+  }
 }
 
 void cTexture::free() {
-    if (mTexture != NULL) {
-        SDL_DestroyTexture(mTexture);
-        mTexture = NULL;
-        mWidth = 0;
-        mHeight = 0;
-    }
+  if (mTexture != NULL) {
+    SDL_DestroyTexture(mTexture);
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+  }
 }
 
 void cTexture::render(int x, int y, int w, int h) {
-    SDL_Rect renderQuad = { x, y, w, h };
-    SDL_RenderCopy(gRenderer, mTexture, NULL, &renderQuad);
+  SDL_Rect renderQuad = {x, y, w, h};
+  SDL_RenderCopy(gRenderer, mTexture, NULL, &renderQuad);
 }
 
-int cTexture::getWidth() {
-    return mWidth;
-}
+int cTexture::getWidth() { return mWidth; }
 
-int cTexture::getHeight() {
-    return mHeight;
-}
+int cTexture::getHeight() { return mHeight; }
 
-int WinMain(int argc, char* argv[]) {
-    initialise();
-    loadAssets();
+int WinMain(int argc, char *argv[]) {
+  initialise();
+  loadAssets();
 
-    bool stop = false;
-    SDL_Event e;
+  bool stop = false;
+  SDL_Event e;
 
-    while (!stop) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                stop = true;
-            } else if (e.type == SDL_KEYDOWN) {
-                SDL_Keycode pressedKeyRaw = e.key.keysym.sym; // I have absulutly no idea what this does but it kinda works (?)
-                const char* pressedKey = SDL_GetKeyName(pressedKeyRaw);
+  while (!stop) {
+    while (SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT) {
+        stop = true;
+      } else if (e.type == SDL_KEYDOWN) {
+        SDL_Keycode pressedKeyRaw =
+            e.key.keysym.sym;  // I have absulutly no idea what this
+                               // does but it kinda works (?)
+        const char *pressedKey = SDL_GetKeyName(pressedKeyRaw);
 
-                printf("Key pressed: %s\n", pressedKey);
-            }
-        }
-
-        // Game logic
-        /*
-        Operator = randNum(1,4);
-        switch (Operator) {
-            case 1:
-                printf("+");
-                break;
-            case 2:
-                printf("-");
-                break;
-            case 3:
-                printf("*");
-                break;
-            case 4:
-                printf("/");
-                break;
-        }
-        */
-        // Graphical rendering
-        SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
-        SDL_RenderClear(gRenderer);
-        gTestBackground.render(0, 0, SCR_WIDTH, SCR_HEIGHT);
-        SDL_RenderPresent(gRenderer);
-
-        // Sounds
-        if (Mix_PlayingMusic() == 0) {
-            Mix_PlayMusic(sMusic, -1);
-        }
+        printf("Key pressed: %s\n", pressedKey);
+      }
     }
 
-    quit();
-    return 0;
+    // Game logic
+    /*
+    Operator = randNum(1,4);
+    switch (Operator) {
+        case 1:
+            printf("+");
+            break;
+        case 2:
+            printf("-");
+            break;
+        case 3:
+            printf("*");
+            break;
+        case 4:
+            printf("/");
+            break;
+    }
+    */
+    // Graphical rendering
+    SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
+    SDL_RenderClear(gRenderer);
+    gTestBackground.render(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    SDL_RenderPresent(gRenderer);
+
+    // Sounds
+    if (Mix_PlayingMusic() == 0) {
+      Mix_PlayMusic(sMusic, -1);
+    }
+  }
+
+  quit();
+  return 0;
 }
 
 void initialise() {
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-    IMG_Init(IMG_INIT_PNG); // Currently only the png format is needed
-    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+  IMG_Init(IMG_INIT_PNG);  // Currently only the png format is needed
+  Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 
-    // Get the current display mode of the primary display
-    SDL_DisplayMode display_mode;
-    if (SDL_GetCurrentDisplayMode(0, &display_mode) != 0) {
-        std::cerr << "SDL_GetCurrentDisplayMode failed: " << SDL_GetError() << std::endl;
-        exit(1);
-    }
+  // Get the current display mode of the primary display
+  SDL_DisplayMode display_mode;
+  if (SDL_GetCurrentDisplayMode(0, &display_mode) != 0) {
+    std::cerr << "SDL_GetCurrentDisplayMode failed: " << SDL_GetError()
+              << std::endl;
+    exit(1);
+  }
 
-    SCR_WIDTH = display_mode.w;
-    SCR_HEIGHT = display_mode.h;
+  SCR_WIDTH = display_mode.w;
+  SCR_HEIGHT = display_mode.h;
 
-    gWindow = SDL_CreateWindow(("MathOrDeath " + VERSION).c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCR_WIDTH, SCR_HEIGHT, SDL_WINDOW_SHOWN);
-    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); // Accelerated with VSync activated
-    SDL_SetWindowFullscreen(gWindow, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+  gWindow = SDL_CreateWindow(("MathOrDeath " + VERSION).c_str(),
+                             SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                             SCR_WIDTH, SCR_HEIGHT, SDL_WINDOW_SHOWN);
+  gRenderer = SDL_CreateRenderer(
+      gWindow, -1,
+      SDL_RENDERER_ACCELERATED |
+          SDL_RENDERER_PRESENTVSYNC);  // Accelerated with VSync activated
+  SDL_SetWindowFullscreen(gWindow, SDL_WINDOW_FULLSCREEN_DESKTOP);
+  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
 }
 
 void loadAssets() {
-    gTestBackground.loadFromFile("res/img/background/test-0001.png");   
-    sMusic = Mix_LoadMUS("res/sfx/music/test.ogg");
+  gTestBackground.loadFromFile("res/img/background/test-0001.png");
+  sMusic = Mix_LoadMUS("res/sfx/music/test.ogg");
 }
 
 void quit() {
-    // Graphical elements
-    gTestBackground.free();
+  // Graphical elements
+  gTestBackground.free();
 
-    // Sounds
-    Mix_FreeMusic(sMusic);
+  // Sounds
+  Mix_FreeMusic(sMusic);
 
-    // SDL Elements
-    SDL_DestroyRenderer(gRenderer);
-    SDL_DestroyWindow(gWindow);
+  // SDL Elements
+  SDL_DestroyRenderer(gRenderer);
+  SDL_DestroyWindow(gWindow);
 
-    /*
-     *  Special attention: If IMG_Quit does NOT work when working with CMake, change #include "close_code.h" on line 2191 to 
-     *  #include <close_code.h>. This will typically resolve the issue. 
-    */  
-    IMG_Quit();
-    Mix_Quit();
-    SDL_Quit();
+  /*
+   *  Special attention: If IMG_Quit does NOT work when working with CMake,
+   * change #include "close_code.h" on line 2191 to #include <close_code.h>.
+   * This will typically resolve the issue.
+   */
+  IMG_Quit();
+  Mix_Quit();
+  SDL_Quit();
 
-    Operator = NULL;
+  Operator = NULL;
 }
--- src/random.cpp
+++ src/random.cpp
@@ -1,9 +1,9 @@
 #include "random.h"
 
 int randNum(int min, int max) {
-    std::random_device rd; // Random device
-    std::mt19937 gen(rd()); // Using the 32-bit Mersenne Twister Generator
-    std::uniform_int_distribution<> distr(min, max);
+  std::random_device rd;   // Random device
+  std::mt19937 gen(rd());  // Using the 32-bit Mersenne Twister Generator
+  std::uniform_int_distribution<> distr(min, max);
 
-    return distr(gen);
+  return distr(gen);
 }
