b830ce1389e5da9b7b9645a8b4ed7042cedec06d
grubbauer <raphael.grubbauer@hotmail.com>
2024-08-31 14:49:36 +0200

    Updated format

--- src/generate_equation.cpp
+++ src/generate_equation.cpp
@@ -3,25 +3,25 @@
 #include "random.h"
 
 std::string randEquation(int lvl) {
-  int rawOp = randNum(1, 4);
-  int firstNum = randNum(lvl, (lvl * 2));
-  int secondNum = randNum(lvl, (lvl * 2));
-  char op;
+	int rawOp = randNum(1, 4);
+	int firstNum = randNum(lvl, (lvl * 2));
+	int secondNum = randNum(lvl, (lvl * 2));
+	char op;
 
-  switch (rawOp) {
-    case 1:
-      op = '+';
-      break;
-    case 2:
-      op = '-';
-      break;
-    case 3:
-      op = '*';
-      break;
-    case 4:
-      op = '/';
-      break;
-  }
+	switch (rawOp) {
+		case 1:
+			op = '+';
+			break;
+		case 2:
+			op = '-';
+			break;
+		case 3:
+			op = '*';
+			break;
+		case 4:
+			op = '/';
+			break;
+	}
 
-  return std::to_string(firstNum) + " " + op + " " + std::to_string(secondNum);
+	return std::to_string(firstNum) + " " + op + " " + std::to_string(secondNum);
 }
