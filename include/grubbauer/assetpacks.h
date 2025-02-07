49ab82081934c4df301b9ea3f48a1677a1c6a51f
grubbauer <raphael.grubbauer@hotmail.com>
2025-02-07 20:12:57 +0100

    Added assetpacks-function files

---
+++ include/grubbauer/assetpacks.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Raphael Grubbauer
+ * Licensed under the Grubbauer Open Source License (GOSL) v1.4.0
+ * See LICENSE.md file in the project root for full license information.
+*/
+
+#ifndef GRUBBAUER_ASSETSPACK_H
+#define GRUBBAUER_ASSETSPACK_H
+
+namespace grubbauer {
+  bool checkForAssetpack();
+}
+
+#endif
---
+++ src/assetpacks.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Raphael Grubbauer
+ * Licensed under the Grubbauer Open Source License (GOSL) v1.4.0
+ * See LICENSE.md file in the project root for full license information.
+*/
+
+#include "grubbauer/assetpacks.h"
+
+namespace grubbauer
+{
+  bool checkForAssetpacks() {
+    // TODO
+  }
+} 
