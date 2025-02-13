d33fa0aecc9790228ebd733702282fc85caa4b7b
grubbauer <raphael.grubbauer@hotmail.com>
2025-02-13 21:27:19 +0100

    Updated installer

---
+++ src/installer.cpp
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2024-2025 Raphael G. Grubbauer / GSP
+ * This file is part of MathOrDeath and is subject to the terms of the End User
+ * License Agreement (EULA).
+ * Unauthorized use, modification, or distribution is prohibited.
+ * See the EULA for details.
+*/
