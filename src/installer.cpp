d473c282f1fb43937e48c006dc4d468377161c20
grubbauer <raphael.grubbauer@hotmail.com>
2025-02-14 17:02:14 +0100

    Deleted installer script

--- src/installer.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (C) 2024-2025 Raphael G. Grubbauer / GSP
- * This file is part of MathOrDeath and is subject to the terms of the End User
- * License Agreement (EULA).
- * Unauthorized use, modification, or distribution is prohibited.
- * See the EULA for details.
-*/
