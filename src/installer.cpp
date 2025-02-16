dd0ff64513ecc814a394942432b35f31e048b0e3
grubbauer <raphael.grubbauer@hotmail.com>
2025-02-16 15:14:10 +0100

    Added installer main source file

---
+++ src/installer.cpp
@@ -0,0 +1,8 @@
+/*
+ * Name:        src/installer.cpp
+ * Purpose:     Installer main file
+ * Author:      Raphael G. Grubbauer
+ * Created:     16.02.2025
+ * Copyright:   (c) 2025 Raphael G. Grubbauer / GSP
+ * License:     MathOrDeath EULA
+*/
