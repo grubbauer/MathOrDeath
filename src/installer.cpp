fcf1b1c0fe2682fe187cca5212c4f0ab9a6dcb45
grubbauer <raphael.grubbauer@hotmail.com>
2025-02-16 15:44:20 +0100

    Removed installer file to bundle into seperate application project git repo

--- src/installer.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Name:        src/installer.cpp
- * Purpose:     Installer main file
- * Author:      Raphael G. Grubbauer
- * Created:     16.02.2025
- * Copyright:   (c) 2025 Raphael G. Grubbauer / GSP
- * License:     MathOrDeath EULA
-*/
